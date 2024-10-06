64b6c9edaa3548326015906c038ae73d28ad8301
Frédéric LASNIER <fred@ouetquand.biz>
2024-10-06 12:50:34 +0200

    first function

--- main.ts
+++ main.ts
@@ -1,23 +1,26 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import {App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting} from 'obsidian';
 
 // Remember to rename these classes and interfaces!
 
-interface MyPluginSettings {
-	mySetting: string;
+interface PluginConfiguration {
+  sourceFolder: string;
+  plop: string;
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+const DEFAULT_SETTINGS: PluginConfiguration = {
+  sourceFolder: 'default',
+  plop: 'plop'
 }
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+export default class PluginLast3lier extends Plugin {
+  settings: PluginConfiguration;
 
 	async onload() {
+    console.log('loading plugin');
 		await this.loadSettings();
 
 		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
+		const ribbonIconEl = this.addRibbonIcon('dice', 'Last3lier Plugin', (evt: MouseEvent) => {
 			// Called when the user clicks the icon.
 			new Notice('This is a notice!');
 		});
@@ -28,18 +31,24 @@ export default class MyPlugin extends Plugin {
 		const statusBarItemEl = this.addStatusBarItem();
 		statusBarItemEl.setText('Status Bar Text');
 
+    this.addCommand({
+      id: 'last3lier-show-file-lines',
+      name: 'Shown for a fle N lines',
+      callback: () => this.showFileLines('path/to/your/file.txt', 5),
+    });
+
 		// This adds a simple command that can be triggered anywhere
 		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
+      id: 'open-last3lier-modal-simple',
+      name: 'Open last3lier modal (simple)',
 			callback: () => {
-				new SampleModal(this.app).open();
+        new Last3lierModal(this.app).open();
 			}
 		});
 		// This adds an editor command that can perform some operation on the current editor instance
 		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
+      id: 'last3lier-editor-command',
+      name: 'Last3lier editor command',
 			editorCallback: (editor: Editor, view: MarkdownView) => {
 				console.log(editor.getSelection());
 				editor.replaceSelection('Sample Editor Command');
@@ -47,8 +56,8 @@ export default class MyPlugin extends Plugin {
 		});
 		// This adds a complex command that can check whether the current state of the app allows execution of the command
 		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
+      id: 'open-last3lier-modal-complex',
+      name: 'Open last3lier modal (complex)',
 			checkCallback: (checking: boolean) => {
 				// Conditions to check
 				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
@@ -56,7 +65,7 @@ export default class MyPlugin extends Plugin {
 					// If checking is true, we're simply "checking" if the command can be run.
 					// If checking is false, then we want to actually perform the operation.
 					if (!checking) {
-						new SampleModal(this.app).open();
+            new Last3lierModal(this.app).open();
 					}
 
 					// This command will only show up in Command Palette when the check function returns true
@@ -66,7 +75,7 @@ export default class MyPlugin extends Plugin {
 		});
 
 		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
+    this.addSettingTab(new Last3lierSettingTab(this.app, this));
 
 		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
 		// Using this function will automatically remove the event listener when this plugin is disabled.
@@ -79,7 +88,7 @@ export default class MyPlugin extends Plugin {
 	}
 
 	onunload() {
-
+    console.log('unloading plugin');
 	}
 
 	async loadSettings() {
@@ -89,16 +98,23 @@ export default class MyPlugin extends Plugin {
 	async saveSettings() {
 		await this.saveData(this.settings);
 	}
+
+  async showFileLines(filePath: string, count:number) {
+    const fileContent = await this.app.vault.read(filePath);
+    const lines = fileContent.split('\n').slice(0, count);
+    new Notice(`First ${count} lines of ${filePath}:\n${lines.join('\n')}`);
+  }
 }
 
-class SampleModal extends Modal {
+class Last3lierModal extends Modal {
 	constructor(app: App) {
 		super(app);
 	}
 
 	onOpen() {
 		const {contentEl} = this;
-		contentEl.setText('Woah!');
+    const msg = 'Ici le test de la modale'
+		contentEl.setText(msg);
 	}
 
 	onClose() {
@@ -107,10 +123,10 @@ class SampleModal extends Modal {
 	}
 }
 
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
+class Last3lierSettingTab extends PluginSettingTab {
+  plugin: PluginLast3lier;
 
-	constructor(app: App, plugin: MyPlugin) {
+  constructor(app: App, plugin: PluginLast3lier) {
 		super(app, plugin);
 		this.plugin = plugin;
 	}
@@ -120,15 +136,26 @@ class SampleSettingTab extends PluginSettingTab {
 
 		containerEl.empty();
 
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
+    new Setting(containerEl)
+      .setName('Setting #1')
+      .setDesc('It\'s a secret')
+      .addText(text => text
+        .setPlaceholder('Enter your secret')
+        .setValue(this.plugin.settings.sourceFolder)
+        .onChange(async (value) => {
+          this.plugin.settings.sourceFolder = value;
+          await this.plugin.saveSettings();
+        }));
+
+    new Setting(containerEl)
+      .setName('Plop #1')
+      .setDesc('It\'s a plop')
+      .addText(text => text
+        .setPlaceholder('Enter your plop')
+        .setValue(this.plugin.settings.plop)
+        .onChange(async (value) => {
+          this.plugin.settings.sourceFolder = value;
+          await this.plugin.saveSettings();
+        }));
 	}
 }
